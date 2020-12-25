ed21d1934a9e68947244c46a023cc0f9292ae34a
Velina Barova <velinabarova@gmail.com>
2020-12-25 08:25:58 +0200

    Bootstrap theme integrated

--- Web/TimeBox.Web.ViewModels/BlogPost/CreateBlogPostInputModel.cs
+++ Web/TimeBox.Web.ViewModels/BlogPost/CreateBlogPostInputModel.cs
@@ -9,12 +9,15 @@
     {
         [Required]
         [MinLength(5)]
+        [Display(Name = "Заглавие")]
         public string Title { get; set; }
 
         [Required]
         [MinLength(20)]
+        [Display(Name = "Текст на публикация")]
         public string BlogText { get; set; }
 
+        [Display(Name = "Снимки")]
         public IEnumerable<IFormFile> Images { get; set; }
     }
 }
--- Web/TimeBox.Web.ViewModels/Quote/CreateQuoteInputModel.cs
+++ Web/TimeBox.Web.ViewModels/Quote/CreateQuoteInputModel.cs
@@ -5,9 +5,11 @@
     public class CreateQuoteInputModel
     {
         [Required]
+        [Display(Name = "Цитат")]
         public string QuoteText { get; set; }
 
         [Required]
+        [Display(Name = "Автор")]
         public string QuoteAuthor { get; set; }
     }
 }
